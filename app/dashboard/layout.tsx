60e231131ba3d715ddedfe98e11508c87f8ce686
Eruki <malasanedison1@gmail.com>
2025-03-30 23:57:16 +0800

    added shadcn button and menu icon from lucid

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
 import DashboardNavigation from "../components/dashboard/DashboardNavigation";
-import { Sheet } from "@/components/ui/sheet";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { MenuIcon } from "lucide-react";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
@@ -9,7 +11,13 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
         <nav className="hidden font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <DashboardNavigation />
         </nav>
-        <Sheet></Sheet>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button>
+              <MenuIcon className="h-5 w-5" />
+            </Button>
+          </SheetTrigger>
+        </Sheet>
       </header>
     </div>
   );
