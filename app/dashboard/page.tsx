a4884b1e3d1cda48077a24d3809e5b5d4fbfd3fb
Eruki <malasanedison1@gmail.com>
2025-04-02 01:10:52 +0800

    added card

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,24 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DollarSign } from "lucide-react";
 import React from "react";
 
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Hello from the dashboard route</h1>
-    </div>
+    <>
+      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between pb-2">
+            <CardTitle>Total Revenue</CardTitle>
+            <DollarSign className="h-4 w-4 text-green-500"></DollarSign>
+          </CardHeader>
+          <CardContent>
+            <p className="text-2xl font-bold">$100.000</p>
+            <p className="text-xs text-muted-foreground">
+              Based on 100 Charges
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 }
