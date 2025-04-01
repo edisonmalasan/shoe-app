8e05e3672e3961e930b3e1a3aa0ffe7d9be46df9
Eruki <malasanedison1@gmail.com>
2025-04-02 02:47:28 +0800

    added card for transaction chart and recent sales

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,18 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { DollarSign, PartyPopper, ShoppingBag, User2 } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback } from "@radix-ui/react-avatar";
+import {
+  DollarSign,
+  PartyPopper,
+  PhilippinePeso,
+  ShoppingBag,
+  User2,
+} from "lucide-react";
 import React from "react";
 
 export default function Dashboard() {
@@ -9,10 +22,10 @@ export default function Dashboard() {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between pb-2">
             <CardTitle>Total Revenue</CardTitle>
-            <DollarSign className="h-4 w-4 text-green-500"></DollarSign>
+            <PhilippinePeso className="h-4 w-4 text-green-500"></PhilippinePeso>
           </CardHeader>
           <CardContent>
-            <p className="text-2xl font-bold">$100.000</p>
+            <p className="text-2xl font-bold">₱100.000</p>
             <p className="text-xs text-muted-foreground">
               Based on 100 Charges
             </p>
@@ -50,6 +63,37 @@ export default function Dashboard() {
             <p className="text-xs text-muted-foreground">
               Total Users Signed Up
             </p>
+          </CardContent>
+        </Card>
+      </div>
+
+      <div className="grid gap-4 md:gp-8 lg:grid-cols-2 xl:grid-cols-3 mt-10">
+        <Card className="xl:col-span-2">
+          <CardHeader>
+            <CardTitle>Transactions</CardTitle>
+            <CardDescription>
+              Recent Transactions from your Store
+            </CardDescription>
+          </CardHeader>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Recent Sales</CardTitle>
+            <CardDescription>Recent Products from your Store</CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-8">
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden sm:flex h-9 w-9">
+                <AvatarFallback>JM</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-medium">John Doe</p>
+                <p className="text-sm text-muted-foreground">
+                  TestEmail@gmail.com
+                </p>
+              </div>
+              <p>+₱200</p>
+            </div>
           </CardContent>
         </Card>
       </div>
