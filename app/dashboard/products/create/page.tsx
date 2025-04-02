add12b2a3175228c3fe8f6e2e8f1c80afbda132d
Eruki <malasanedison1@gmail.com>
2025-04-03 02:55:12 +0800

    changes

--- app/dashboard/products/create/page.tsx
+++ app/dashboard/products/create/page.tsx
@@ -6,11 +6,12 @@ export default function CreateProductRoute() {
   return (
     <form>
       <div className="flex items-center gap-4">
-        <Button /*variant={"outline"} */ size={"icon"} asChild>
+        <Button variant={"outline"} size={"icon"} asChild>
           <Link href={"/dashboard/products"}>
             <ChevronLeft className="w-4 h-4" />
           </Link>
         </Button>
+        <h1 className="font-semibold text-xl">New Product</h1>
       </div>
     </form>
   );
